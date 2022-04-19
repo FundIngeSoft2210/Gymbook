d319fb52886d4b9b6374b7d428f3bdaa043b35ac
GermanVelasco06 <julio06.gv@gmail.com>
2022-04-18 23:48:12 -0500

    Update login.java

--- Gymbook/src/login/login.java
+++ Gymbook/src/login/login.java
@@ -28,31 +28,70 @@ import login.Clases.cliente;
  * @author julio
  */
 public class login {
-    private HashMap<Integer,Usuario> lista_De_Usuarios= new HashMap <>();
+    private HashMap<String,Usuario> lista_De_Usuarios= new HashMap <>();
     private List<cliente> lista_de_clientes= new ArrayList<>();
     private List<Entrenadores> lista_de_entrenadores = new ArrayList<>();
     private List<Gymnasio> lista_de_gimnasios = new ArrayList<>();
     
 
-    public HashMap<Integer, Usuario> getLista_De_Usuarios() {
+    public HashMap<String, Usuario> getLista_De_Usuarios() {
         return lista_De_Usuarios;
     }
 
-    public void setLista_De_Usuarios(HashMap<Integer, Usuario> lista_De_Usuarios) {
+    public void setLista_De_Usuarios(HashMap<String, Usuario> lista_De_Usuarios) {
         this.lista_De_Usuarios = lista_De_Usuarios;
     }
-    public void insertarUsuario(String username,String contrasena){
+    public String insertarUsuario(String username,String contrasena){
         Usuario nuevoUsuario = new Usuario(username,contrasena);
-        this.lista_De_Usuarios.put(this.lista_De_Usuarios.size()+1,nuevoUsuario);
+        if (!this.existe_el_usuario(username, contrasena)){
+            this.lista_De_Usuarios.put(username,nuevoUsuario);
+            return "Usuario Insertado correctamente";
+        }
+        else {
+            return "Usuario ya existente";
+        }
     }
-    public void insertarCliente(String gimnasio, String nombre, String apellido, long numero_Telefono, Date fechaNacimiento, int edad, String direccion, String ocupacion, String EPS, double peso, double altura, String nombre_Contacto_emergencia, long numero_telefono_emergencia, String username, String contrasena, Icon imagen){
+    public String insertarCliente(String gimnasio, String nombre, String apellido, long numero_Telefono, Date fechaNacimiento, int edad, String direccion, String ocupacion, String EPS, double peso, double altura, String nombre_Contacto_emergencia, long numero_telefono_emergencia, String username, String contrasena, Icon imagen){
         cliente nuevoCliente = new cliente(gimnasio,nombre,apellido, numero_Telefono, fechaNacimiento, edad, direccion, ocupacion, EPS, peso, altura, nombre_Contacto_emergencia, numero_telefono_emergencia, username, contrasena,imagen);
-        this.lista_de_clientes.add(nuevoCliente);
+        if (!this.existe_el_usuario(username, contrasena)){
+            this.lista_de_clientes.add(nuevoCliente);
+        }
+        return this.insertarUsuario(username, contrasena);
+    }
+    public String insertarEntrenador(String nombre, String descripcion, String username, String contrasena, Icon imagen){
+        Entrenadores nuevoEntrenador = new Entrenadores(nombre,descripcion,username, contrasena, imagen);
+        if (!this.existe_el_usuario(username, contrasena))
+            this.lista_de_entrenadores.add(nuevoEntrenador);
+        return this.insertarUsuario(username, contrasena);
+    }
+    
+    public String insertarGimnasio(String Nombre_del_Gimnasio, String Direccion, String username, String contrasena, Icon imagen){
+        Gymnasio nuevoGimnasio = new Gymnasio(Nombre_del_Gimnasio,Direccion, username, contrasena, imagen);
+        if (!this.existe_el_usuario(username, contrasena))
+            this.lista_de_gimnasios.add(nuevoGimnasio);
+        return this.insertarUsuario(username, contrasena);
+    }
+    public void eliminarUsuario(String username){
+        this.lista_de_clientes.remove(this.lista_De_Usuarios.get(username));
+        this.lista_de_entrenadores.remove(this.lista_De_Usuarios.get(username));
+        this.lista_de_gimnasios.remove(this.lista_De_Usuarios.get(username));
+        this.lista_De_Usuarios.remove(username);
     }
     
     boolean existe_el_usuario(String username,String contrasena){
         Usuario usuarioARevisar = new Usuario(username,contrasena);
-        return this.lista_De_Usuarios.containsValue(usuarioARevisar);
+        if (this.lista_De_Usuarios.containsKey(username) || this.lista_De_Usuarios.containsValue(usuarioARevisar)){
+            return true;
+        }
+        return false;
+    }
+    
+    boolean ContrasenaCorrecta(String username,String contrasena){
+        if (existe_el_usuario(username,contrasena))
+                if (contrasena == this.lista_De_Usuarios.get(username).getContrasena())
+                    return true;
+        
+        return false;
     }
     
     void guardarTodo() throws FileNotFoundException,IOException {
@@ -79,7 +118,7 @@ public class login {
         FileInputStream ArchivoAEscribir = new FileInputStream("UsuariosYContrasenas");
         ObjectInputStream entrada = new ObjectInputStream(ArchivoAEscribir);
         
-        this.lista_De_Usuarios.putAll((Map<? extends Integer, ? extends Usuario>) entrada.readObject());
+        this.lista_De_Usuarios.putAll((Map<? extends String, ? extends Usuario>) entrada.readObject());
     }
     
     void guardarEntrenadores() throws FileNotFoundException, IOException{
