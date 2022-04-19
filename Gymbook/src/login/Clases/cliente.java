9d499bb9e9f8e77b6c97aed45ece45eb34b739ee
GermanVelasco06 <julio06.gv@gmail.com>
2022-04-18 20:14:27 -0500

    Create cliente.java

---
+++ Gymbook/src/login/Clases/cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login.Clases;
+
+/**
+ *
+ * @author julio
+ */
+public class cliente extends Usuario{
+    
+    public cliente(String username, String contrasena) {
+        super(username, contrasena);
+    }
+    private String gimnasio;
+    
+}
