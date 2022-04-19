491cf4ebeb4279bfda5ee8afb6fc1413e03f3c7a
GermanVelasco06 <julio06.gv@gmail.com>
2022-04-18 20:14:30 -0500

    Create Entrenadores.java

---
+++ Gymbook/src/login/Clases/Entrenadores.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login.Clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author julio
+ */
+public class Entrenadores extends Usuario{
+    
+    public Entrenadores(String username, String contrasena) {
+        super(username, contrasena);
+    }
+    
+    public Entrenadores(String username, String contrasena,String nombre,String descripcion) {
+        super(username, contrasena);
+        this.nombre= nombre;
+        this.descripcion=descripcion;
+    }
+    
+    private List<cliente> Entrenados = new ArrayList<>();
+    private String nombre;
+    private String descripcion; 
+
+    public List<cliente> getEntrenados() {
+        return Entrenados;
+    }
+
+    public void setEntrenados(List<cliente> Entrenados) {
+        this.Entrenados = Entrenados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }    
+}
