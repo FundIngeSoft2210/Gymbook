e7a4e9d20749e0358cc2a74f8c33b21ebacd649d
Maikol01990 <54080930+Maikol01990@users.noreply.github.com>
2022-05-12 12:33:22 -0500

    Add files via upload

---
+++ Gymbook/src/login/Clases/Metas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login.Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author MAIKOL
+ */
+public class Metas {
+    
+    private String idMeta;
+    private String nombremeta;
+    private Date fechaFin;
+    private String areaGanancia;
+    private int PuntosGanancia;
+    private String estadometa;
+    private String tipometa;
+}
